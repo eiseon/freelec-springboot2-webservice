4fce79ca8cafee37d992c1a61e7b37c34eaec93b
sk.a <sk.a@bomapp.com>
2020-12-24 16:27:56 +0900

    머스테치로 화면 구성하기 > 전체 조회화면 만들기

--- src/main/java/org/jojoidu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/jojoidu/book/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,11 @@
 package org.jojoidu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("select p from Posts p ORDER BY p.id desc")
+    List<Posts> findAllDesc();
 }
--- src/main/java/org/jojoidu/book/springboot/service/posts/PostsService.java
+++ src/main/java/org/jojoidu/book/springboot/service/posts/PostsService.java
@@ -3,11 +3,16 @@ package org.jojoidu.book.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.jojoidu.book.springboot.domain.posts.Posts;
 import org.jojoidu.book.springboot.domain.posts.PostsRepository;
+import org.jojoidu.book.springboot.web.dto.PostsListResponseDto;
 import org.jojoidu.book.springboot.web.dto.PostsResponseDto;
 import org.jojoidu.book.springboot.web.dto.PostsSaveRequestDto;
 import org.jojoidu.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +39,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }

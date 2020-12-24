a847fd7b2ba6f6ddd58f39adf9224ac05f70e9bc
sk.a <sk.a@bomapp.com>
2020-12-24 18:01:44 +0900

    머스테치로 화면 구성하기 > 게시글 수정, 삭제 화면 만들기

--- src/main/java/org/jojoidu/book/springboot/service/posts/PostsService.java
+++ src/main/java/org/jojoidu/book/springboot/service/posts/PostsService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -46,5 +45,13 @@ public class PostsService {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
